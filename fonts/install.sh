e240d145537a3088f68d61933daabb6400f5ac00
Eric Roch <eric@roch.us>
2020-04-23 03:34:45 -0400

    Squash history of caarlos0/dotfiles into single commit

---
+++ fonts/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+install() {
+	curl -L -s -o "$1/SourceCodePro-Light.ttf" \
+		https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Light.ttf
+}
+
+if [ "$(uname -s)" = "Darwin" ]; then
+	if command -v brew >/dev/null 2>&1; then
+		brew tap homebrew/cask-fonts
+		brew cask install font-source-code-pro
+	else
+		install ~/Library/Fonts
+	fi
+else
+	mkdir -p ~/.fonts
+	install ~/.fonts
+fi
