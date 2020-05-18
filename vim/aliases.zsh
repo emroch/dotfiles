bf30410744eb8f8e1666e95991649e6c159ac7e2
Eric Roch <eric@roch.us>
2020-05-17 22:47:38 -0400

    Adding aliases

--- vim/aliases.zsh
+++ vim/aliases.zsh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # neovim
-if command -v nvim >/dev/null 2>&2; then
+if command -v nvim >/dev/null 2>&1; then
     alias vim='nvim'
 fi
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,9 @@
 #!/bin/sh
 alias reload!='exec "$SHELL" -l'
+
+if command -v htop >/dev/null 2>&1; then
+    alias top='htop'
+fi
+if command -v ripgrep >/dev/null 2>&1; then
+    alias grep='ripgrep'
+fi
