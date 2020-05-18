0f74535a46d65ef896bb4a864b8609bb2ac0bb6f
Eric Roch <eric@roch.us>
2020-05-18 02:22:27 -0400

    Add computer name set up, install iterm shell integration

--- iterm/install.sh
+++ iterm/install.sh
@@ -4,3 +4,5 @@ sed "s;/Users/emroch;$HOME;g" \
     "$DOTFILES"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES"/iterm/com.googlecode.iterm2.plist
 defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES/iterm"
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
+
+curl -sL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash >/dev/null
